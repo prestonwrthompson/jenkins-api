f7f1d8d31cfe1048308b05378c1d12b887ebc555
Preston Thompson <preston@appboy.com>
2018-09-19 19:35:49 -0400

    add script for bash in jenkins and change running jenkins for jobs that last after container closes

--- run-jenkins-in-docker.sh
+++ run-jenkins-in-docker.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-#from https://hub.docker.com/r/jenkinsci/blueocean/
-docker run -p 8080:8080 jenkinsci/blueocean
+# from https://jenkins.io/doc/book/installing/
+docker run \
+  -u root \
+  --rm \
+  -d \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --name jenkins-blueocean \
+  jenkinsci/blueocean
